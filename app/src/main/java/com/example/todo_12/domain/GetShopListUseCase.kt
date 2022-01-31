667f5733f143981e34d1f8736318b3d9df432f9b
ir-sen <72728441+ir-sen@users.noreply.github.com>
2022-01-31 15:06:33 +0300

    Create first use case GetShopListUseCase.kt

---
+++ app/src/main/java/com/example/todo_12/domain/GetShopListUseCase.kt
@@ -0,0 +1,8 @@
+package com.example.todo_12.domain
+
+class GetShopListUseCase {
+
+    fun getShopList(): List<ShopItem> {
+        TODO()
+    }
+}
--- app/src/main/java/com/example/todo_12/presentation/MainActivity.kt
+++ app/src/main/java/com/example/todo_12/presentation/MainActivity.kt
@@ -1,11 +1,15 @@
-package com.example.todo_12
+package com.example.todo_12.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.todo_12.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
 }
