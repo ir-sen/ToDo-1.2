16c1f48a7612c9745471321021ae215e691738e1
ir-sen <72728441+ir-sen@users.noreply.github.com>
2022-02-04 14:12:31 +0300

    Create layout

---
+++ app/src/main/java/com/example/todo_12/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.example.todo_12.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.todo_12.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
