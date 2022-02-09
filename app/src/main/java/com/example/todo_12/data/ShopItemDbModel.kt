0dd1c6be2d14a9381e45a015696bc9633c30ded7
ir-sen <72728441+ir-sen@users.noreply.github.com>
2022-02-09 15:14:25 +0300

    add basic RoomDataBase components Entity Dao Database

---
+++ app/src/main/java/com/example/todo_12/data/ShopItemDbModel.kt
@@ -0,0 +1,14 @@
+package com.example.todo_12.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "shop_items")
+data class ShopItemDbModel(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val name: String,
+    val count: Int,
+    val enable: Boolean
+) {
+}
